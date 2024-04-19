7d5d60fc593f90a2f535aea6f6b7f7e812b276e0
Jemima Bueno <109461467+jemimabueno@users.noreply.github.com>
2024-04-19 16:59:19 -0300

    Update ex2.java

--- exs_estrutura_sequencial/src/ex2.java
+++ exs_estrutura_sequencial/src/ex2.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 public class ex2 {
 
 	public static void main(String[] args) {
-		//Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro casas decimais conforme exemplos.
-		//Fórmula da área: area = π . raio2
-		//Considere o valor de π = 3.141598
+		
+//Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro casas decimais conforme exemplos.
+//Fórmula da área: area = π . raio2
+//Considere o valor de π = 3.141598
 		
 		Scanner sc = new Scanner(System.in);
 		double pi = 3.141598;
