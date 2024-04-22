9272e06d61027c4f5f00ae5d8840828bc1d801b9
Jemima Bueno <109461467+jemimabueno@users.noreply.github.com>
2024-04-22 14:46:15 -0300

    Add files via upload

---
+++ exs_estrutura_repetitiva/src/ex2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ex2 {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int x,y = sc.nextInt();
+		
+		
+		
+		
+		sc.close();
+	}
+
+}
