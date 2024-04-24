c6d9987d745c62c5ac161daa85feca56afaf4def
Jemima Bueno <109461467+jemimabueno@users.noreply.github.com>
2024-04-24 16:02:00 -0300

    Update ex_for_7.java

--- exs_estruturas_repetitivas/para(for)/src/ex_for_7.java
+++ exs_estruturas_repetitivas/para(for)/src/ex_for_7.java
@@ -4,9 +4,9 @@ public class ex_for_7 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		// Fazer um programa para ler um número inteiro positivo N. O programa deve então mostrar na tela N linhas,
-		//começando de 1 até N. Para cada linha, mostrar o número da linha, depois o quadrado e o cubo do valor, conforme
-		//exemplo.
+		/* Fazer um programa para ler um número inteiro positivo N. O programa deve então mostrar na tela N linhas,
+		começando de 1 até N. Para cada linha, mostrar o número da linha, depois o quadrado e o cubo do valor, conforme
+		exemplo*/
 
 		int n = sc.nextInt();
 		for(int i=1;i<n;i++) {
