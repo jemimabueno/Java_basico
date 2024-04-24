ad27190fe3481234a4025438317d34ed9d47dc98
Jemima Bueno <109461467+jemimabueno@users.noreply.github.com>
2024-04-24 16:00:58 -0300

    Update ex_for_2.java

--- exs_estruturas_repetitivas/para(for)/src/ex_for_2.java
+++ exs_estruturas_repetitivas/para(for)/src/ex_for_2.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class ex_for_2 {
 
 	public static void main(String[] args) {
-		// Leia um valor inteiro N. Este valor será a quantidade de valores inteiros X que serão lidos em seguida.
-		//Mostre quantos destes valores X estão dentro do intervalo [10,20] e quantos estão fora do intervalo, mostrando
-		//essas informações conforme exemplo (use a palavra "in" para dentro do intervalo, e "out" para fora do intervalo).
+		/* Leia um valor inteiro N. Este valor será a quantidade de valores inteiros X que serão lidos em seguida.
+		Mostre quantos destes valores X estão dentro do intervalo [10,20] e quantos estão fora do intervalo, mostrando
+		essas informações conforme exemplo (use a palavra "in" para dentro do intervalo, e "out" para fora do intervalo).*/
 		
 		Scanner sc = new Scanner(System.in);
 	
