81626cfee5149630b082bc6395de3c9a7d37d633
Jemima Bueno <109461467+jemimabueno@users.noreply.github.com>
2024-04-24 15:59:59 -0300

    Update ex8.java

--- exs_estrutura_condicional/src/ex8.java
+++ exs_estrutura_condicional/src/ex8.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class ex8 {
 
 	public static void main(String[] args) {
-		// Em um país imaginário denominado Lisarb, todos os habitantes ficam felizes em pagar seus impostos, pois sabem
-		//que nele não existem políticos corruptos e os recursos arrecadados são utilizados em benefício da população, sem
-		//qualquer desvio. A moeda deste país é o Rombus, cujo símbolo é o R$.
-		//Leia um valor com duas casas decimais, equivalente ao salário de uma pessoa de Lisarb. Em seguida, calcule e
-		//mostre o valor que esta pessoa deve pagar de Imposto de Renda, segundo a tabela abaixo.
+		/* Em um país imaginário denominado Lisarb, todos os habitantes ficam felizes em pagar seus impostos, pois sabem
+		que nele não existem políticos corruptos e os recursos arrecadados são utilizados em benefício da população, sem
+		qualquer desvio. A moeda deste país é o Rombus, cujo símbolo é o R$.
+	        Leia um valor com duas casas decimais, equivalente ao salário de uma pessoa de Lisarb. Em seguida, calcule e
+		mostre o valor que esta pessoa deve pagar de Imposto de Renda, segundo a tabela abaixo. */
 		
 		Scanner sc = new Scanner(System.in);
 		double salario,imposto;
