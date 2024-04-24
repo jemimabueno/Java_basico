a696ff99266cde3b279cafd05fe188202f7ee2a3
Jemima Bueno <109461467+jemimabueno@users.noreply.github.com>
2024-04-24 15:59:40 -0300

    Update ex6.java

--- exs_estrutura_condicional/src/ex6.java
+++ exs_estrutura_condicional/src/ex6.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class ex6 {
 
 	public static void main(String[] args) {
-		// Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos
-		//seguintes intervalos ([0,25], (25,50], (50,75], (75,100]) este valor se encontra. Obviamente se o valor não estiver em
-	//nenhum destes intervalos, deverá ser impressa a mensagem “Fora de intervalo”.
+		/* Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos
+		seguintes intervalos ([0,25], (25,50], (50,75], (75,100]) este valor se encontra. Obviamente se o valor não estiver em
+	nenhum destes intervalos, deverá ser impressa a mensagem “Fora de intervalo”. */
 
 		Scanner sc = new Scanner(System.in);
 		double valor,intervalo;
