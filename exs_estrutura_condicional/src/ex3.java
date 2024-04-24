fcf48965dad89ceeb8ce3f187fe1c3f7d3bc2026
Jemima Bueno <109461467+jemimabueno@users.noreply.github.com>
2024-04-24 15:58:42 -0300

    Update ex3.java

--- exs_estrutura_condicional/src/ex3.java
+++ exs_estrutura_condicional/src/ex3.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class ex3 {
 
 	public static void main(String[] args) {
-		// Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao
-		//Multiplos", indicando se os valores lidos são múltiplos entre si. Atenção: os números devem poder ser digitados em
-		//ordem crescente ou decrescente.
+		
+		/* Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao
+		Multiplos", indicando se os valores lidos são múltiplos entre si. Atenção: os números devem poder ser digitados em
+		ordem crescente ou decrescente.*/
 		
 		Scanner sc = new Scanner(System.in);
 		
