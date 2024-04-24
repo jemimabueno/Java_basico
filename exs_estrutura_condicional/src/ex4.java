18b02406c9cf78e866d511d83c2f37f4fad4dcd9
Jemima Bueno <109461467+jemimabueno@users.noreply.github.com>
2024-04-24 15:59:02 -0300

    Update ex4.java

--- exs_estrutura_condicional/src/ex4.java
+++ exs_estrutura_condicional/src/ex4.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class ex4 {
 
 	public static void main(String[] args) {
-		// Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo pode
-		//começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas.
+		
+		/* Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo pode
+		começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas.*/
 		
 		Scanner sc = new Scanner(System.in);
 		double horaInicial, horaFinal;
