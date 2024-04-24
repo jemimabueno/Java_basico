ca4efc560743da395ed8c2c7d18503df2533d3e2
Jemima Bueno <109461467+jemimabueno@users.noreply.github.com>
2024-04-24 16:00:44 -0300

    Update ex_for_1.java

--- exs_estruturas_repetitivas/para(for)/src/ex_for_1.java
+++ exs_estruturas_repetitivas/para(for)/src/ex_for_1.java
@@ -4,8 +4,8 @@ public class ex_for_1 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		//Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o
-		//X, se for o caso.
+		/*Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o
+		X, se for o caso.*/
 		
 		int x = sc.nextInt();
 			
