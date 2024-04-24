45194784bdd585e12a32d8a4fba73e48db691928
Jemima Bueno <109461467+jemimabueno@users.noreply.github.com>
2024-04-24 16:01:31 -0300

    Update ex_for_4.java

--- exs_estruturas_repetitivas/para(for)/src/ex_for_4.java
+++ exs_estruturas_repetitivas/para(for)/src/ex_for_4.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 public class ex_for_4 {
 
 	public static void main(String[] args) {
-		// Fazer um programa para ler um número N. Depois leia N pares de números e mostre a divisão do primeiro pelo
-		//segundo. Se o denominador for igual a zero, mostrar a mensagem "divisao impossivel".
+		/* Fazer um programa para ler um número N. Depois leia N pares de números e mostre a divisão do primeiro pelo
+		segundo. Se o denominador for igual a zero, mostrar a mensagem "divisao impossivel".*/
 		
 		Scanner sc = new Scanner(System.in);
 		
