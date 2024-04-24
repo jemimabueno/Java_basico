8c2e0395caa2f77ea05f17183cda8db60b47a170
Jemima Bueno <109461467+jemimabueno@users.noreply.github.com>
2024-04-24 16:01:18 -0300

    Update ex_for_3.java

--- exs_estruturas_repetitivas/para(for)/src/ex_for_3.java
+++ exs_estruturas_repetitivas/para(for)/src/ex_for_3.java
@@ -3,10 +3,10 @@ public class ex_for_3 {
 
 	public static void main(String[] args) {
 		
-		// Leia 1 valor inteiro N, que representa o número de casos de teste que vem a seguir. Cada caso de teste consiste
-		//de 3 valores reais, cada um deles com uma casa decimal. Apresente a média ponderada para cada um destes
-		//conjuntos de 3 valores, sendo que o primeiro valor tem peso 2, o segundo valor tem peso 3 e o terceiro valor tem
-		//peso 5
+		/* Leia 1 valor inteiro N, que representa o número de casos de teste que vem a seguir. Cada caso de teste consiste
+		de 3 valores reais, cada um deles com uma casa decimal. Apresente a média ponderada para cada um destes
+	        conjuntos de 3 valores, sendo que o primeiro valor tem peso 2, o segundo valor tem peso 3 e o terceiro valor tem
+		peso 5*/
 		
 		Scanner sc = new Scanner(System.in);
 		
