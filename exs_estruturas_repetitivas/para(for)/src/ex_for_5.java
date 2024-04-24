aecd008dc77ec4fc7ed02fc335f53c20310e4471
Jemima Bueno <109461467+jemimabueno@users.noreply.github.com>
2024-04-24 16:01:41 -0300

    Update ex_for_5.java

--- exs_estruturas_repetitivas/para(for)/src/ex_for_5.java
+++ exs_estruturas_repetitivas/para(for)/src/ex_for_5.java
@@ -3,8 +3,8 @@ public class ex_for_5 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		// Ler um valor N. Calcular e escrever seu respectivo fatorial. Fatorial de N = N * (N-1) * (N-2) * (N-3) * ... * 1.
-		//Lembrando que, por definição, fatorial de 0 é 1.
+		/* Ler um valor N. Calcular e escrever seu respectivo fatorial. Fatorial de N = N * (N-1) * (N-2) * (N-3) * ... * 1.
+		Lembrando que, por definição, fatorial de 0 é 1.*/
 		
 		int n = sc.nextInt();
 		
