b05433d9cdaa5e2bd4bb18b91a3896af2938e6c0
Jemima Bueno <109461467+jemimabueno@users.noreply.github.com>
2024-04-24 16:00:21 -0300

    Update ex1.java

--- exs_estruturas_repetitivas/enquanto(while)/src/ex1.java
+++ exs_estruturas_repetitivas/enquanto(while)/src/ex1.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class ex1 {
 
 	public static void main(String[] args) {
-		// Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha
-		//incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser
-		//impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002
+		/* Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha
+		incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser
+		impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002*/
 
 		Scanner sc = new Scanner(System.in);
 		int senha = sc.nextInt();
