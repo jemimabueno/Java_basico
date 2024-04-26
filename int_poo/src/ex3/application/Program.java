953fdbfc7a83c5db5f667fa63dbe8b2f1876618f
Jemima Bueno <109461467+jemimabueno@users.noreply.github.com>
2024-04-26 09:37:12 -0300

    Add files via upload

---
+++ int_poo/src/ex3/application/Program.java
@@ -0,0 +1,38 @@
+package application;
+import java.util.Scanner;
+import entities.Product;
+
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		/* Mostrarosdadosdoproduto(nome,preço,quantidadenoestoque,valortotalno
+		 estoque)
+	     •Realizarumaentradanoestoqueemostrarnovamenteosdadosdoproduto
+	     •Realizarumasaídanoestoqueemostrarnovamenteosdadosdoproduto     */
+		
+		Scanner sc= new Scanner(System.in);
+		Product pdt = new Product();
+		
+		System.out.println("Enter product data:");
+		pdt.name = sc.next();
+		pdt.price = sc.nextDouble();
+		pdt.quantity  = sc.nextInt();
+		
+		System.out.println("Product data:" + pdt.name  +  ", " + " $"+ pdt.price + ", " + pdt.quantity + " units, " + "Total: $" + pdt.TotalValueInStock());
+
+		System.out.println("Enter the number of products to be added in stock:");
+		int quantity = sc.nextInt();
+		pdt.AddProducts(quantity);
+		
+		System.out.println("Updated data:" + pdt.name + "," + pdt.price + ", " + pdt.quantity + " units, " + "Total: $ " + pdt.TotalValueInStock());
+		System.out.println("Enter the number of products to be removed from stock:");
+		
+		quantity = sc.nextInt();
+		pdt.RemoveProducts(quantity);
+		System.out.println("Updated data:" + pdt.name + "," + pdt.price + ", " + pdt.quantity + " units, " + "Total: $ " + pdt.TotalValueInStock());
+		sc.close();
+	}
+
+}
