147c8a5c33beea3eef15036de432383a90e4da41
Jemima Bueno <109461467+jemimabueno@users.noreply.github.com>
2024-04-29 13:54:41 -0300

    Add files via upload

--- POO/membros_estaticos/ex1/src/util/CurrencyConverter.java
+++ POO/membros_estaticos/ex1/src/util/CurrencyConverter.java
@@ -1,1 +1,10 @@
+package util;
 
+public class CurrencyConverter {
+	
+	public static double IOF = 0.06;
+
+	public static double conv(double dollarPrice, double amount) {
+		return dollarPrice * amount * (1.0 + IOF);
+	}
+}
