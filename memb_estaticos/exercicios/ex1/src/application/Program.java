fb71ba94bcf5c32f0137ba3bc2719a4e7088cb92
Jemima Bueno <109461467+jemimabueno@users.noreply.github.com>
2024-04-29 08:50:45 -0300

    Add files via upload

--- memb_estaticos/exercicios/ex1/src/application/Program.java
+++ memb_estaticos/exercicios/ex1/src/application/Program.java
@@ -1,1 +1,19 @@
+package application;
+import java.util.Scanner;
+import util.CurrencyConverter;
+public class Program {
 
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("What is the dollar price?");
+		double dollarPrice = sc.nextDouble();
+		System.out.println("How many dollar will be bought?");
+		double amount = sc.nextDouble();
+		double total = CurrencyConverter.conv(amount, dollarPrice);
+		
+		System.out.println("Amount to be paid in reais:"  + total);
+	}
+
+}
