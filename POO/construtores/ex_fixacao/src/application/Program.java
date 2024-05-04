964e771b982f7d819f1f1bd1a1a72bc735ec3b82
Jemima Bueno <109461467+jemimabueno@users.noreply.github.com>
2024-05-04 07:19:12 -0300

    Add files via upload

--- POO/construtores/ex_fixacao/src/application/Program.java
+++ POO/construtores/ex_fixacao/src/application/Program.java
@@ -1,1 +1,44 @@
+package application;
+import java.util.Scanner;
+import entities.Account;
+public class Program {
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		Account account;
+		
+		System.out.print("Enter account number: ");
+		int number = sc.nextInt();
+		System.out.println("Enter account holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Is there an initial deposit (y/n)? ");
+		char response = sc.next().charAt(0);
+		if(response == 'y') {
+			System.out.print("Enter initial deposit value:");
+			double initialDeposit = sc.nextDouble();
+			account = new Account(number,holder,initialDeposit);
+		}
+		else {
+			account = new Account(number,holder);
+		}
+		
+		System.out.println();
+		System.out.println("Enter a deposit value: ");
+		double depositValue = sc.nextDouble();
+		account.deposit(depositValue);
+		System.out.println("Updated account data: ");
+		System.out.println(account);
+		
+		System.out.println();
+		System.out.println("Enter a withdraw value: ");
+		double withdrawValue = sc.nextDouble();
+		account.withdraw(withdrawValue);
+		System.out.println("Updated account data: ");
+		System.out.println(account);
+		
+		sc.close();
+	}
+
+}
